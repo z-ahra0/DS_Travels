0eda11a4b1094df904ebef8729446791ea3dc36f
z-ahra0 <aminuzahra5@gmail.com>
2025-01-15 18:29:46 +0100

    commit 9

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('home/',views.home,name='home'),
+    path('/',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('services/',views.services,name='services'),
