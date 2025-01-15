02d9efd411bac12cd29cbc45928603764a6883e3
z-ahra0 <aminuzahra5@gmail.com>
2025-01-15 17:46:17 +0100

    commit 6

--- DS_Travels/settings.py
+++ DS_Travels/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
